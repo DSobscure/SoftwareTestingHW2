5ddebe87269b24816bd7cba9a520544739a1ad3e
DSobscure <snbruce13@yahoo.com.tw>
2017-05-02 00:08:46 +0800

    spec-based triangle problem test pass

--- ProblemLibrary/Problem.cs
+++ ProblemLibrary/Problem.cs
@@ -6,7 +6,32 @@ namespace ProblemLibrary
     {
         public static bool TriangleProblem(int sideA, int sideB, int sideC, out string resultMessage)
         {
-            throw new NotImplementedException("Problem TriangleProblem");
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                resultMessage = "邊長不合法";
+                return false;
+            }
+            else if (sideA >= sideB + sideC || sideB >= sideA + sideC || sideC >= sideA + sideB)
+            {
+                resultMessage = "兩邊和不大於第三邊";
+                return false;
+            }
+            else if (sideA == sideB && sideB == sideC)
+            {
+                resultMessage = "正三角形";
+                return true;
+            }
+            else if (sideA == sideB || sideB == sideC || sideA == sideC)
+            {
+                resultMessage = "等腰三角形";
+                return true;
+            }
+            else
+            {
+                int maxSide = Math.Max(sideA, Math.Max(sideB, sideC));
+                resultMessage = (maxSide * maxSide == sideA * sideA + sideB * sideB + sideC * sideC - maxSide * maxSide) ? "直角三角形" : "普通三角形";
+                return true;
+            }
         }
 
         public static bool NextDateProblem(int year, int month, int day, out string resultMessage)
