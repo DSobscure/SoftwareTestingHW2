1ddb5a18c45e074922ad0579687793854f197e12
DSobscure <snbruce13@yahoo.com.tw>
2017-05-01 22:37:27 +0800

    Problem API define

---
+++ ProblemLibrary/Problem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProblemLibrary
+{
+    public static class Problem
+    {
+        public static bool TriangleProblem(int sideA, int sideB, int sideC, out string resultMessage)
+        {
+            throw new NotImplementedException("Problem TriangleProblem");
+        }
+
+        public static bool NextDateProblem(int month, int day, int year, out string resultMessage)
+        {
+            throw new NotImplementedException("Problem NextDateProblem");
+        }
+
+        public static bool CommissionProblem(int lockCount, int stockCount, int barrelCount, out string resultMessage)
+        {
+            throw new NotImplementedException("Problem CommissionProblem");
+        }
+    }
+}
