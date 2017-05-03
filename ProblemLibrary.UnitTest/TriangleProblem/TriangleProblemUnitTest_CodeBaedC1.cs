dd3fa11646b6f5b1c0fcb26434823f17bca43bd0
DSobscure <snbruce13@yahoo.com.tw>
2017-05-03 18:07:01 +0800

    triangle problem c1 test pass

--- ProblemLibrary.UnitTest/TriangleProblem/TriangleProblemUnitTest_CodeBaedC1.cs
+++ ProblemLibrary.UnitTest/TriangleProblem/TriangleProblemUnitTest_CodeBaedC1.cs
@@ -9,7 +9,44 @@ namespace ProblemLibrary.UnitTest.TriangleProblem
         [TestMethod]
         public void TP_C1TestMethod1()
         {
-            throw new System.NotImplementedException();
+            string resultMessage;
+            Assert.IsFalse(Problem.TriangleProblem(0, 0, 0, out resultMessage));
+            Assert.AreEqual("邊長不合法", resultMessage);
+        }
+        [TestMethod]
+        public void TP_C1TestMethod2()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.TriangleProblem(2, 1, 1, out resultMessage));
+            Assert.AreEqual("兩邊和不大於第三邊", resultMessage);
+        }
+        [TestMethod]
+        public void TP_C1TestMethod3()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.TriangleProblem(2, 2, 2, out resultMessage));
+            Assert.AreEqual("正三角形", resultMessage);
+        }
+        [TestMethod]
+        public void TP_C1TestMethod4()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.TriangleProblem(2, 2, 1, out resultMessage));
+            Assert.AreEqual("等腰三角形", resultMessage);
+        }
+        [TestMethod]
+        public void TP_C1TestMethod5()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.TriangleProblem(3, 4, 5, out resultMessage));
+            Assert.AreEqual("直角三角形", resultMessage);
+        }
+        [TestMethod]
+        public void TP_C1TestMethod6()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.TriangleProblem(3, 4, 6, out resultMessage));
+            Assert.AreEqual("普通三角形", resultMessage);
         }
     }
 }
