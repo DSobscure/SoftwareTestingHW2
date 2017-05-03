89b16088eb0041389ac9e5c23872c921532cda7f
DSobscure <snbruce13@yahoo.com.tw>
2017-05-03 17:01:46 +0800

    NextDate C0 test pass

--- ProblemLibrary.UnitTest/NextDateProblem/NextDateProblemUnitTest_CodeBasedC0.cs
+++ ProblemLibrary.UnitTest/NextDateProblem/NextDateProblemUnitTest_CodeBasedC0.cs
@@ -9,7 +9,65 @@ namespace ProblemLibrary.UnitTest.NextDateProblem
         [TestMethod]
         public void NDP_C0TestMethod1()
         {
-            throw new System.NotImplementedException();
+            string resultMessage;
+            Assert.IsFalse(Problem.NextDateProblem(2017, 1, 0, out resultMessage));
+            Assert.AreEqual("日期不合法", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod2()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.NextDateProblem(2017, 12, 31, out resultMessage));
+            Assert.AreEqual("2018/1/1", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod3()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.NextDateProblem(2017, 2, 0, out resultMessage));
+            Assert.AreEqual("日期不合法", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod4()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.NextDateProblem(2017, 2, 28, out resultMessage));
+            Assert.AreEqual("2017/3/1", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod5()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.NextDateProblem(2020, 2, 0, out resultMessage));
+            Assert.AreEqual("日期不合法", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod6()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.NextDateProblem(2020, 2, 29, out resultMessage));
+            Assert.AreEqual("2020/3/1", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod7()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.NextDateProblem(2017, 4, 0, out resultMessage));
+            Assert.AreEqual("日期不合法", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod8()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.NextDateProblem(2017, 4, 30, out resultMessage));
+            Assert.AreEqual("2017/5/1", resultMessage);
+        }
+        [TestMethod]
+        public void NDP_C0TestMethod9()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.NextDateProblem(2017, 0, 1, out resultMessage));
+            Assert.AreEqual("日期不合法", resultMessage);
         }
     }
 }
