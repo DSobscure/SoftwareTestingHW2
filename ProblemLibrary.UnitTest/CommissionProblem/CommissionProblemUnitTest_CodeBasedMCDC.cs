46c1634ec211dc45c07a0251387e476bffb0967c
DSobscure <snbruce13@yahoo.com.tw>
2017-05-03 18:26:13 +0800

    commission problem mcdc test pass

--- ProblemLibrary.UnitTest/CommissionProblem/CommissionProblemUnitTest_CodeBasedMCDC.cs
+++ ProblemLibrary.UnitTest/CommissionProblem/CommissionProblemUnitTest_CodeBasedMCDC.cs
@@ -7,9 +7,67 @@ namespace ProblemLibrary.UnitTest.CommissionProblem
     public class CommissionProblemUnitTest_CodeBasedMCDC
     {
         [TestMethod]
-        public void CP_MCDCTestMethod1()
+        public void CP_MCDC_TestMethod1()
         {
-            throw new System.NotImplementedException();
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(0, 0, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod2()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.CommissionProblem(1, 1, 1, out resultMessage));
+            Assert.AreEqual("10", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod3()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.CommissionProblem(10, 20, 10, out resultMessage));
+            Assert.AreEqual("195", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod4()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.CommissionProblem(10, 5, 48, out resultMessage));
+            Assert.AreEqual("270", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod5()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(71, 0, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod6()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 0, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod7()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 81, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod8()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 1, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_MCDC_TestMethod9()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 1, 91, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
         }
     }
 }
