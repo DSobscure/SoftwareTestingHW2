065b5c0d4ed87b47f442fef05988474ea4ec46f1
DSobscure <snbruce13@yahoo.com.tw>
2017-05-03 18:22:12 +0800

    commission problem c2 test pass

--- ProblemLibrary.UnitTest/CommissionProblem/CommissionProblemUnitTest_CodeBasedC2.cs
+++ ProblemLibrary.UnitTest/CommissionProblem/CommissionProblemUnitTest_CodeBasedC2.cs
@@ -9,7 +9,65 @@ namespace ProblemLibrary.UnitTest.CommissionProblem
         [TestMethod]
         public void CP_C2TestMethod1()
         {
-            throw new System.NotImplementedException();
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(0, 0, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod2()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.CommissionProblem(1, 1, 1, out resultMessage));
+            Assert.AreEqual("10", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod3()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.CommissionProblem(10, 20, 10, out resultMessage));
+            Assert.AreEqual("195", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod4()
+        {
+            string resultMessage;
+            Assert.IsTrue(Problem.CommissionProblem(10, 5, 48, out resultMessage));
+            Assert.AreEqual("270", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod5()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(71, 0, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod6()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 0, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod7()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 81, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod8()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 1, 0, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
+        }
+        [TestMethod]
+        public void CP_C2TestMethod9()
+        {
+            string resultMessage;
+            Assert.IsFalse(Problem.CommissionProblem(1, 1, 91, out resultMessage));
+            Assert.AreEqual("輸入不合法", resultMessage);
         }
     }
 }
